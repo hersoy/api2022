dc2b2df3d3561150a21ead1f8cf58851680fc308
Hamnza Ersoy <hersoy11@gmail.com>
2022-07-13 23:20:55 +0300

    13.07.2022

---
+++ src/test/java/test_data/jsonplaceHolderTestData.java
@@ -0,0 +1,15 @@
+package test_data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class jsonplaceHolderTestData {
+    public Map<String,Object> expectedDataWithAllKeys(Integer userId, String title, Boolean completed){
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId",userId);
+        expectedData.put("title", title);
+        expectedData.put("completed",completed);
+
+        return expectedData;
+    }
+}
